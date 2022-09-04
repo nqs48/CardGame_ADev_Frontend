eba7e730e388e74362ecda283272868f4a60d4e8
Nestor Quiroga <nqs48@icloud.com>
2022-09-04 16:54:43 -0500

    Creating Reactive Form <Jugadores>

--- src/app/modules/game/pages/new-game/new-game.component.ts
+++ src/app/modules/game/pages/new-game/new-game.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-game',
   templateUrl: './new-game.component.html',
-  styleUrls: ['./new-game.component.css']
+  styleUrls: ['./new-game.component.css'],
 })
 export class NewGameComponent implements OnInit {
+  frmJugadores: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.frmJugadores = this.createFormJugadores();
   }
 
+  private createFormJugadores(): FormGroup {
+    return new FormGroup({
+      jugadores: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  public submitJugadores(){
+    console.log("Submit: " ,this.frmJugadores.getRawValue());
+  };
+
+  ngOnInit(): void {}
 }
